8a966d64718d00fc61c60137ee5bf4f5d1b5b8a8
Parth Pathak <parth.d.pathak@ey.com>
2019-04-08 18:06:42 -0400

    spliting-app-modules

---
+++ src/app/app-config/config.constant.ts
@@ -0,0 +1,9 @@
+export const AppRoutes = Object.freeze({
+  home: 'home',
+  login: 'public/login',
+  unAuthenticatedPage: 'public/home',
+});
+
+export const ApiRoutes = Object.freeze({
+
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/* Application Imports */
+
+/* Modules */
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { PublicModule } from './public/public.module';
 import { AppRoutingModule } from './app-routing.module';
+import { NavigationModule } from './navigation/navigation.module';
+/* Components */
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +20,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule,
+    SharedModule,
+    PublicModule,
+    NavigationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
